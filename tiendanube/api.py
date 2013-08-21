c89327eb58b4ca420927e78234f7b0055a203347
Juan Catalano <catalanojuan@gmail.com>
2013-08-21 03:52:51 +0000

    Refactored http client requests method to support variable args.

--- tiendanube/api.py
+++ tiendanube/api.py
@@ -11,15 +11,20 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 class APIClient(object):
     API_VERSION = 'v1'
     API_ENDPOINT = 'https://api.tiendanube.com'
+    ARGS = ['resource_id', 'subresource', 'subresource_id']
 
     def __init__(self, api_key, user_agent):
         headers = {
-            'Authentication': 'bearer {}'.format(api_key), 
+            'Authentication': 'bearer {}'.format(api_key),
             'User-Agent': user_agent
         }
         self.headers = headers
 
-    def make_request(self, id, resource, endpoint=None, verb='GET', extra=None):
+    def get_options(self, args):
+        return [args[k] for k in self.ARGS if k in args and args[k]]
+
+    def make_request(self, id, resource, **kwargs):
+        verb = kwargs.get('verb', 'GET')
         method = getattr(requests, verb.lower())
         url = furl(self.API_ENDPOINT)
         url.path.segments = [
@@ -30,10 +35,9 @@ class APIClient(object):
         if resource:
             url.path.segments.append(resource)
 
-        if endpoint:
-            url.path.segments.append(endpoint)
+        url.path.segments.extend(self.get_options(kwargs))
 
-        url.args = extra
-        
+        url.args = kwargs.get('extra')
+
         logging.debug('URL: {}'.format(str(url)))
         return method(str(url), headers=self.headers)
