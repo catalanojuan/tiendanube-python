af7fd62f2356f3d6953bb38ffb4c34efe6e4abcf
Juan Catalano <catalanojuan@gmail.com>
2013-08-21 04:17:00 +0000

    Style convention fix: tab to spaces convertion.

--- tiendanube/resources/decorators.py
+++ tiendanube/resources/decorators.py
@@ -3,19 +3,19 @@ from .base import ListSubResource
 
 
 def subresources(subresource_names):
-	def _decorated(klass):
-		orig_get = klass.get
+    def _decorated(klass):
+        orig_get = klass.get
 
-		def get_wrapper(self, id):
-			obj = orig_get(self, id)
-			for subresource in subresource_names:
-				setattr(
-					obj,
-					subresource,
-					ListSubResource(self, id, subresource)
-				)
-			return obj
+        def get_wrapper(self, id):
+            obj = orig_get(self, id)
+            for subresource in subresource_names:
+                setattr(
+                    obj,
+                    subresource,
+                    ListSubResource(self, id, subresource)
+                )
+            return obj
 
-		klass.get = get_wrapper
-		return klass
-	return _decorated
+        klass.get = get_wrapper
+        return klass
+    return _decorated
