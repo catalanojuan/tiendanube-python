572fcba028bfdf478a41668d37fe355c1b2f9fa4
Juan Catalano <catalanojuan@gmail.com>
2013-08-25 06:16:21 +0000

    Added required dependencies to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = 'tiendanube-python',
+    name = 'tiendanube',
+    packages = [
+        'tiendanube',
+        'tiendanube.resources',
+    ],
     version = '1.0',
     description = '',
     license = 'MIT',
@@ -18,8 +22,18 @@ setup(
     author = 'Juan Catalano',
     author_email = 'catalanojuan@gmail.com',
 
-    py_modules = ['tiendanube'],
-    install_requires = ['requests'],
+    install_requires = [
+        "argparse==1.2.1",
+        "bunch==1.0.1",
+        "furl==0.3.4",
+        "ipython==1.0.0",
+        "mock==1.0.1",
+        "orderedmultidict==0.7.1",
+        "py==1.4.15",
+        "pytz==2013b",
+        "requests==1.2.3",
+        "wsgiref==0.1.2",
+    ],
 
     classifiers = (
         'Development Status :: 4 - Beta',
